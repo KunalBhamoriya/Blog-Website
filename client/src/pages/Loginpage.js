18daaf33b3e16562b513c272c9027fce93752082
KunalBhamoriya <135410677+KunalBhamoriya@users.noreply.github.com>
2024-08-12 22:21:19 +0530

    created pages

--- client/src/pages/Loginpage.js
+++ client/src/pages/Loginpage.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import Form from 'antd'
 
 
 const Loginpage = () => {
   return (
     <>
-      <div className="register-page">
-                <Form layout='vertical' className='border border-5 p-5 log-reg-Card' onFinish={submitHandler}>
-                    <h1>Login to Teck Light</h1>
-                    <Form.Item label = "Email" name="email">
-                        <Input type='email' required />
-                    </Form.Item>
-                    <Form.Item label = "Password" name="password">
-                        <Input type='password' required/>
-                    </Form.Item>
-                    <div className="d-flex justify-content-between">
-                        <Link to="/register" className='text-danger'>Not a user ? Click to Register</Link>
-                        <button className='btn log-res-button ms-3'>Login</button>
-                    </div>
-                </Form>  
-            </div>
+      <div>Login</div>
     </>
   )
 }
